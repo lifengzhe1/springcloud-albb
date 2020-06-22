a5b792b366eb5d8f3a9d7016783423af42287b03
lifengzhe1 <67252943+lifengzhe1@users.noreply.github.com>
2020-06-22 14:54:32 +0900

    update test
    update test

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
 public class test{
- pulic static void main(String[] args){
-   System.out.println("holle word");
- }
-
+   pulic static void main(String[] args){
+    System.out.println("holle word");
+  }
 }
